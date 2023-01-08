cdea354fb375012bbe3195a3675ba0b3dd697d6c
A-l-e-x0001 <119552561+A-l-e-x0001@users.noreply.github.com>
2023-01-07 22:41:31 -0600

    Made more simple
    removed any extra lines of code

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,4 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'Alex'
-description 'Spawn'
-version '1.0.0'
-
-resource_type 'gametype' { name = 'SpawnScript' }
-
-client_script 'SpawnScript.lua'
+client_script 'startspawn.lua'
