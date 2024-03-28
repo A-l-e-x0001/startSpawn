58195231eab21b67d95815f1a500cb37f48368ac
Alex <119552561+A-l-e-x0001@users.noreply.github.com>
2024-03-27 21:55:40 -0500

    Update SpawnScript.lua

--- SpawnScript.lua
+++ SpawnScript.lua
@@ -1,21 +1,36 @@
-local spawnPos = vector3(-425.51, 1123.56, 325.85)
+-- spawnMangaer
+local spawnPos = math.random(1,#config.Spawns)
+function RandomSpawn()
+    spawnPos = math.random(1,#config.Spawns)
+end
+local count = 0;
 
 AddEventHandler('onClientGameTypeStart', function()
     exports.spawnmanager:setAutoSpawnCallback(function()
         exports.spawnmanager:spawnPlayer({
-            x = spawnPos.x,
-            y = spawnPos.y,
-            z = spawnPos.z,
-            model = 'a_m_y_skater_01'
+            x = config.Spawns[spawnPos].pos.x,
+            y = config.Spawns[spawnPos].pos.y,
+            z = config.Spawns[spawnPos].pos.z,
+            model = 'a_m_m_skater_01',   
         }, function()
-            TriggerEvent('chat:addMessage', {
-                args = { "Welcome To Alex's dev server!" }
-            })
+            if count == 0 then
+                TriggerEvent('chat:addMessage', {
+                    args = { 'Welcome to Alex\'s party!', }
+                })
+                count = count + 1;
+            end
+            RandomSpawn()
         end)
     end)
-      
     exports.spawnmanager:setAutoSpawn(true)
     exports.spawnmanager:forceRespawn()
     Citizen.Wait(500)
     exports.spawnmanager:setAutoSpawn(false)
+end)
+
+
+RegisterCommand('lobby', function(source)
+    local playerPed = PlayerPedId()
+    local lobbySpawn = config.Spawns[math.random(1,#config.Spawns)]
+    SetEntityCoords(playerPed,lobbySpawn.pos)
 end)
