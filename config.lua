a89cb4959941f5f624128a11455e510bccf28765
Alex <119552561+A-l-e-x0001@users.noreply.github.com>
2024-03-27 21:56:10 -0500

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,9 @@
+config = {}
+
+config.Spawns = {
+    {name = 'Spawn1', pos = vector3(-425.45,1123.53,325.85)},
+    {name = 'Spawn2',pos = vector3(-412.6,1126.18,325.9)},
+    {name = 'Spawn3', pos = vector3(-432.9,1132.01,325.9)},
+    {name = 'Spawn4',pos = vector3(-424.5, 1166.03,325.9)},
+    {name = 'Spawn4',pos = vector3(-405.82,1160.64,325.92)}
+}
